cac101b9887efaa03b90e54a3ce57084c181c852
rkj13780 <rakeshonrediff@gmail.com>
2017-11-20 16:41:10 +0530

    test

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World -Rakesh you are tisting" );
+        System.out.println( "Hello World Rakesh you are tisting" );
     }
 
 	public int calculateSomething() {
